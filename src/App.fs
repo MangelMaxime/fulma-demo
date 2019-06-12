75b2439bde7b7139abc1a9f287647ce8abe8a3ae
Maxime Mangel <maxime.shadaen@protonmail.com>
2019-06-12 16:55:55 +0200

    WIP

--- src/App.fs
+++ src/App.fs
@@ -5,9 +5,10 @@ open Fable.React
 open Fable.React.Props
 open State
 open Types
-open Fulma
 open Fable.FontAwesome
 open Fable.FontAwesome.Free
+open Fulma
+open Fulma.Extensions.Wikiki
 
 let private navbarEnd =
     Navbar.End.div [ ]
@@ -57,27 +58,114 @@ let private navbarView isBurgerOpen dispatch =
                       Navbar.Item.a [ ]
                         [ str "Logout" ] ] ] ] ]
 
+let item txt icon isActive =
+    Menu.Item.li [ Menu.Item.IsActive isActive ]
+        [ Icon.icon [ ]
+            [ Fa.i [ icon ]
+                [ ] ]
+          str txt ]
 let sideMenu =
-    Menu.menu [ ]
+    Menu.menu [ CustomClass "sidebar-main" ]
         [ Menu.list [ ]
-            [ Menu.Item.li [ ]
-                [ Icon.icon [ ]
-                    [ Fa.i [ Fa.Solid.Inbox ]
-                        [ ] ]
-                  str "Inbox" ]
-              Menu.Item.li [ ] [ str "Sent" ]
-              Menu.Item.li [ ] [ str "Stared" ]
-              Menu.Item.li [ ] [ str "Trash" ] ] ]
+            [ item "Inbox" Fa.Solid.Inbox true
+              item "Sent" Fa.Regular.Envelope false
+              item "Stared" Fa.Solid.Star false
+              item "Trash" Fa.Regular.TrashAlt false ] ]
+
+let email =
+    Media.media [ Media.CustomClass "is-email-preview" ]
+        [ Media.left [ ]
+            [ Checkradio.checkbox [ Checkradio.Id "checkradio-1"
+                                    Checkradio.Size IsSmall ]
+                [ ] ]
+          Media.content [ ]
+            [ Heading.p [ Heading.Is6 ]
+                [ str "Maxime Mangel" ]
+              Heading.p [ Heading.Is6
+                          Heading.IsSubtitle ]
+                [ str "Lorem ipsum dolor sit amet, consectetur adipiscing" ] ]
+          Media.right [ CustomClass "is-info-container" ]
+            [ span [ Class "is-date" ]
+                [ str "11 Jun 2019" ]
+              Icon.icon [ ]
+                [ Fa.i [ Fa.Regular.Star ]
+                    [ ] ] ] ]
+
+let emailsList =
+    div [ ]
+        [ Media.media [ Media.CustomClass "is-email" ]
+            [ Media.left [ Props [ Style [ Border "1px solid black" ] ] ]
+                [ Checkbox.checkbox [ ]
+                    [ Checkbox.input [ ] ] ]
+              Media.content [ Props [ Style [ Border "1px solid black" ] ] ]
+                [ str "Fable - Newsletter #1" ]
+              Media.right [ Props [ Style [ Border "1px solid black" ] ] ]
+                [ str "11 Jun 2019" ] ] ]
+
+let buttonIcon icon =
+    Button.button [  ]
+        [ Icon.icon [ ]
+            [ Fa.i [ icon ]
+                [ ] ] ]
+
+let menubar =
+    Level.level [ Level.Level.CustomClass "is-menubar"
+                  Level.Level.Modifiers [ Modifier.IsMarginless ] ]
+        [ Level.left [ ]
+            [ Button.list [ Button.List.HasAddons ]
+                [ buttonIcon Fa.Solid.LongArrowAltLeft ]
+              Button.list [ Button.List.HasAddons ]
+                [ buttonIcon Fa.Solid.Eye
+                  buttonIcon Fa.Solid.EyeSlash ]
+              Button.list [ Button.List.HasAddons ]
+                [ buttonIcon Fa.Regular.TrashAlt
+                  buttonIcon Fa.Solid.Archive
+                  buttonIcon Fa.Solid.Ban ] ]
+          Level.right [ ]
+            [ Button.list [ Button.List.HasAddons ]
+                [ buttonIcon Fa.Solid.AngleLeft
+                  buttonIcon Fa.Solid.AngleDown
+                  buttonIcon Fa.Solid.AngleRight ] ] ]
 
 let pageContent =
     Columns.columns [ Columns.CustomClass "is-inbox"
                       Columns.IsGapless ]
-        [ Column.column [ Column.Width (Screen.All, Column.Is2) ]
-            [ sideMenu ]
+        [ Column.column [ Column.CustomClass "is-main-menu"
+                          Column.Width (Screen.All, Column.Is2) ]
+            [ Text.div [ Modifiers [ Modifier.TextAlignment (Screen.All, TextAlignment.Centered) ]
+                         Props [ Style [ Padding "2rem 2rem 1rem" ] ] ]
+                [ Button.button [ Button.Color IsPrimary
+                                  Button.IsFullWidth
+                                  Button.Modifiers [ Modifier.TextWeight TextWeight.Bold ] ]
+                    [ str "Compose" ] ]
+              sideMenu ]
           Column.column [ ]
-            [ ]
-          Column.column [ ]
-            [ ] ]
+            [ menubar
+              Columns.columns [ Columns.IsGapless ]
+                [ Column.column [ Column.CustomClass "is-email-list"
+                                  Column.Width (Screen.All, Column.Is5) ]
+                    [ email
+                      email
+                      email
+                      email
+                      email
+                      email
+                      email
+                      email
+                      email
+                      email
+                      email
+                      email
+                      email
+                      email
+                      email
+                      email
+                      email
+                      email
+                      email
+                      email ]
+                  Column.column [ ]
+                    [ ] ] ] ]
 
 let private root model dispatch =
     div [ ]
